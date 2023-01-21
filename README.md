25893cfac9b495d9bd08725f0b4d3d32035f71b3
Ursulino Costa <71571163+Ursulinocosta@users.noreply.github.com>
2023-01-21 19:36:12 -0300

    Add files via upload

---
+++ README.md
@@ -0,0 +1,18 @@
+# Animeflix
+
+Desafio de recriar a interface da Netflix com alterações
+
+Recriando a interface da Netflix utilizando HTML5, CSS3 e JavaScript. 
+
+Temas abordados nessa aula: como estruturar um layout, técnicas de CSS3 com containers e variáveis, como posicionar os elementos com Flexbox e como utilizar plugins JQuery a favor da aplicação.
+
+- Ultilizando Jquery na criação do carrossel e deixando a inteface responsiva.
+
+## Tecnologias ultilizadas:
+- HTML
+- CSS
+- JavaScript
+- Jquery
+
+![image](https://user-images.githubusercontent.com/71571163/163890642-d2be7174-1b36-499a-a6de-8bbdf8e492d2.png)
+
---
+++ js/setup.js
@@ -0,0 +1,59 @@
+/*Teste */
+/*$('.owl-carousel').owlCarousel({
+    loop:true,
+    margin:20,
+    nav:false,
+    responsive:{
+        0:{
+            items:1
+        },
+        600:{
+            items:3
+        },
+        1000:{
+            items:5
+        }
+    }
+})*/
+
+/*Teste */
+/*$('.owl-carousel').owlCarousel({
+    center: true,
+    items:2,
+    loop:true,
+    margin:15,
+    responsive:{
+        600:{
+            items:5
+        }
+    }
+});
+$('.owl-carousel').owlCarousel({
+    center: true,
+    items:2,
+    loop:false,
+    margin:15,
+    responsive:{
+        600:{
+            items:5
+        }
+    }
+});*/
+
+$('.owl-carousel').owlCarousel({
+stagePadding: 70,
+    loop:true,
+    margin:20,
+    nav:false,
+    responsive:{
+        0:{
+            items:1
+        },
+        600:{
+            items:3
+        },
+        1000:{
+            items:5
+        }
+    }
+})
